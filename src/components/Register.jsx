eab652d5584433d1a8efa360ebb5b3147b9364b9
Amol Chitale <amolchitaledcjr@gmail.com>
2024-10-10 16:37:17 +0530

    Final Commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,46 +6,71 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { setCurrUser } from "../store/userSlice";
 import { useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 
 function Register() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
-  const [username,setUsername]  = useState("");
-  const [password,setPassword] = useState("");
+  const [eyeOpen, setEyeOpen] = useState(false);
+  const dispatch = useDispatch();
 
-  const [eyeOpen,setEyeOpen] = useState(false)
-  const dispatch = useDispatch()
-
-  const handleRegister = async()=>{
+  const handleRegister = async () => {
     const newUser = {
       username,
       password,
-    }
-    const data = await axios.post(registerRoute,newUser)
-    navigate("/")
-    
-    dispatch(setCurrUser(data.data.payload))
-  }
+    };
+    const data = await axios.post(registerRoute, newUser);
+    navigate("/");
+
+    dispatch(setCurrUser(data.data.payload));
+  };
 
   return (
     <div className="outer">
       <div className="register">
         <div className="closeButtonContainer">
-          <div className="close-button" onClick={()=>{
-            navigate("/")
-          }}></div>
+          <div
+            className="close-button"
+            onClick={() => {
+              navigate("/");
+            }}
+          ></div>
         </div>
         <div className="heading">Register</div>
         <div className="username">
           Username
-          <input className="input" placeholder="Enter username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+          <input
+            className="input"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="username">
           Password
-            <input placeholder="Enter password" type={eyeOpen?"text":"password"} value={password} onChange={(e)=>setPassword(e.target.value)}/>
-            <img alt="eye" src={eye} onClick={()=>setEyeOpen(!eyeOpen)}/>
+          <div className="pass">
+            <input
+              placeholder="Enter password"
+              type={eyeOpen ? "text" : "password"}
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <FontAwesomeIcon
+              icon={!eyeOpen ? faEye : faEyeSlash}
+              onClick={() => {
+                setEyeOpen(!eyeOpen);
+              }}
+            />
+          </div>
         </div>
-        <div className="pill" onClick={handleRegister}>Register</div>
+        <div className="pill" onClick={handleRegister}>
+          Register
+        </div>
       </div>
     </div>
   );
